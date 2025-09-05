5b80c865a912cc2054c149e6d6904eb45b937bd1
Pixciv <snackzmind@gmail.com>
2025-09-05 19:35:04 +0300

    Update MainActivity.kt

--- app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.arvinapp.acgb
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -9,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    private var mUploadMessage: ValueCallback<Array<Uri>>? = null
+    private val FILECHOOSER_RESULTCODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +22,40 @@ class MainActivity : AppCompatActivity() {
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
             webViewClient = WebViewClient()
-            webChromeClient = WebChromeClient()
+            webChromeClient = object : WebChromeClient() {
+                // Dosya seçme penceresini açmak için bu metot gereklidir
+                override fun onShowFileChooser(
+                    webView: WebView?,
+                    filePathCallback: ValueCallback<Array<Uri>>?,
+                    fileChooserParams: FileChooserParams?
+                ): Boolean {
+                    if (mUploadMessage != null) {
+                        mUploadMessage!!.onReceiveValue(null)
+                        mUploadMessage = null
+                    }
+                    mUploadMessage = filePathCallback
+                    val intent = fileChooserParams?.createIntent()
+                    startActivityForResult(intent, FILECHOOSER_RESULTCODE)
+                    return true
+                }
+            }
         }
 
         setContentView(webView)
 
         // assets içindeki HTML dosyasını aç
         webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (mUploadMessage == null) {
+                return
+            }
+            mUploadMessage!!.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data))
+            mUploadMessage = null
+        }
     }
 
     // Geri tuşu WebView'de gezinme için
